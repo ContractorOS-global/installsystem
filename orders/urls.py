77e641aa085e09b11edee0888cbbc56249c6b5b1
ContractorOS-global <makhtiyev1@gmail.com>
2026-02-22 18:30:29 +0100

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path("", views.wallet, name="wallet"),
-    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
+    path("", views.wallet_view, name="wallet"),
 ]
