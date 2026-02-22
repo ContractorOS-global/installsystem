a18ea9e122018ec695ae7cc776bf40622961f864
ContractorOS-global <makhtiyev1@gmail.com>
2026-02-22 10:52:06 +0100

    Update urls.py

--- core/core/urls.py
+++ core/core/urls.py
@@ -3,11 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    # Django login/logout/password views:
-    # /accounts/login/  /accounts/logout/  /accounts/password_change/ ...
-    path("accounts/", include("django.contrib.auth.urls")),
-
-    # Your app urls
     path("", include("orders.urls")),
 ]
