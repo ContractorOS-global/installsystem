493df9b1e244d5e0c17da1e92ae6d377835122c3
ContractorOS-global <makhtiyev1@gmail.com>
2026-02-22 08:07:53 +0100

    Update admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,18 +1,1 @@
 from django.contrib import admin
-from .models import (
-    Company,
-    InstallationOrder,
-    Delivery,
-    PenaltyRule,
-    LedgerEntry,
-    OrderAssignment,
-    OrderDocument,
-)
-
-admin.site.register(Company)
-admin.site.register(InstallationOrder)
-admin.site.register(Delivery)
-admin.site.register(PenaltyRule)
-admin.site.register(LedgerEntry)
-admin.site.register(OrderAssignment)
-admin.site.register(OrderDocument)
