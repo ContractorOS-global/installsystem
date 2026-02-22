951f854aa2e4548bf70faf7220267f15f3d9c452
ContractorOS-global <makhtiyev1@gmail.com>
2026-02-22 01:19:20 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -62,13 +62,3 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 import os
-from django.contrib.auth import get_user_model
-
-if os.environ.get("RENDER"):
-    User = get_user_model()
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="admin@example.com",
-            password="admin123"
-        )
