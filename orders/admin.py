dc470cebd0369a2b199bc63627550c508bafd6c2
ContractorOS-global <makhtiyev1@gmail.com>
2026-02-22 09:41:49 +0100

    Update admin.py

--- orders/admin.py
+++ orders/admin.py
@@ -1,47 +1,14 @@
 from django.contrib import admin
 from .models import Company, Transaction
 
-
-# -----------------------
-# COMPANY (Фирмы)
-# -----------------------
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "name",
-        "email",
-        "balance_eur",
-    )
-
-    search_fields = (
-        "name",
-        "email",
-    )
-
+    list_display = ("name", "email", "balance_eur")
+    search_fields = ("name", "email")
     filter_horizontal = ("users",)
 
-
-# -----------------------
-# TRANSACTIONS (Кошелек)
-# -----------------------
 @admin.register(Transaction)
 class TransactionAdmin(admin.ModelAdmin):
-    list_display = (
-        "id",
-        "date",
-        "type",
-        "source",
-        "amount",
-        "order_id",
-    )
-
-    list_filter = (
-        "type",
-        "source",
-    )
-
-    search_fields = (
-        "source",
-        "comment",
-    )
+    list_display = ("date", "type", "source", "amount", "order_id", "comment")
+    list_filter = ("type", "date")
+    search_fields = ("source", "comment")
