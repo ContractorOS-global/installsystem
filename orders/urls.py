b2e531f21f9abe0e965821b4ea3bbc7f95cd906b
ContractorOS-global <makhtiyev1@gmail.com>
2026-02-22 09:49:30 +0100

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import wallet_view
 
 urlpatterns = [
+    # кошелёк
     path("", wallet_view, name="wallet"),
+    path("wallet/", wallet_view, name="wallet"),
+
+    # логин/логаут
+    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="/login/"), name="logout"),
 ]
