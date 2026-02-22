b59b243d35b078578ba0594f36b3693ff64482ce
ContractorOS-global <makhtiyev1@gmail.com>
2026-02-22 18:44:56 +0100

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -5,17 +5,15 @@ from . import views
 urlpatterns = [
     path("", views.wallet_view, name="wallet"),
 
-    path(
-        "login/",
-        auth_views.LoginView.as_view(
-            template_name="registration/login.html"
-        ),
-        name="login",
-    ),
+    # login без слэша
+    path("login", auth_views.LoginView.as_view(
+        template_name="registration/login.html"
+    ), name="login_no_slash"),
 
-    path(
-        "logout/",
-        auth_views.LogoutView.as_view(next_page="login"),
-        name="logout",
-    ),
+    # login со слэшем
+    path("login/", auth_views.LoginView.as_view(
+        template_name="registration/login.html"
+    ), name="login"),
+
+    path("logout/", auth_views.LogoutView.as_view(next_page="/login/"), name="logout"),
 ]
