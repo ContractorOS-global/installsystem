a84b2f243c13b8823aa7f6b0a1499fd0d0b7b5da
ContractorOS-global <makhtiyev1@gmail.com>
2026-02-22 04:57:23 +0100

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -66,3 +66,15 @@ import os
 
 if os.environ.get("RENDER"):
     DEBUG = False
+
+from django.contrib.auth import get_user_model
+
+if os.environ.get("RENDER"):
+    User = get_user_model()
+
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            email="admin@example.com",
+            password="admin123456"
+        )
